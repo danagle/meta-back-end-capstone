a784347fe75f5257834d6135fd644a6f7e016687
Dan Nagle <752694+danagle@users.noreply.github.com>
2025-04-19 18:27:20 +0100

    Add views and URL routes

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,32 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets
 
-# Create your views here.
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuItemSerializer, UserSerializer
+
+
 def index(request):
     return render(request, 'index.html', {})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
