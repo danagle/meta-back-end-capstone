5fa3cec5c92ed2a0ac766a4ad3404f81a9a084a4
Dan Nagle <752694+danagle@users.noreply.github.com>
2025-04-19 17:58:33 +0100

    Add MySQL database connection and models

--- LittleLemon/littlelemon/settings.py
+++ LittleLemon/littlelemon/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -74,13 +75,24 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Load environment variables from file
+mysql_config = BASE_DIR.parent / 'my.env'
+assert(Path(mysql_config).is_file())
+load_dotenv(mysql_config)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': getenv('DJANGO_DB_NAME'),
+        'USER': getenv('DJANGO_DB_USER'),
+        'PASSWORD': getenv('DJANGO_DB_PASSWORD'),
+        'HOST': getenv('DJANGO_DB_HOST'),
+        'PORT': getenv('DJANGO_DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
